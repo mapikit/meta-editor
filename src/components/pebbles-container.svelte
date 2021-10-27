119e71cb23886962e350285c443ea8965193185f
Fábio Meneses Junior <flgmjr@gmail.com>
2021-10-27 11:48:29 -0300

    update pebbles to have random rotation

---
+++ src/components/pebbles-container.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+import { onMount } from "svelte";
+
+  import PebbleAnimation from "./pebble-animation.svelte";
+  import P1 from "./pebbles/p1.svelte";
+  import P2 from "./pebbles/p2.svelte";
+  import P3 from "./pebbles/p3.svelte";
+  import P4 from "./pebbles/p4.svelte";
+  import P5 from "./pebbles/p5.svelte";
+
+  let pebbles = [P1, P5, P4, P5, P4, P2, P1, P3, P2, P3];
+
+  const colors = ["#ffaf35", "#3cf691", "#df263a", "#41cff6", "#171730", "#313151"]
+  let usableColors = [...colors];
+
+  function getColor() {
+    if(usableColors.length === 0) usableColors = [...colors];
+    const randomIndex = Math.floor(Math.random()*usableColors.length);
+    const selected = usableColors[randomIndex];
+    usableColors.splice(randomIndex,1);
+    return selected;
+  }
+
+</script>
+<div class="pebbles-holder">
+  {#if pebbles !== undefined}
+    {#each pebbles as Pebble}
+      <PebbleAnimation xStart={10} xSpread={80} yStart={20} ySpread={20}>
+        <Pebble color="{getColor()}" scale={1}/>
+      </PebbleAnimation>
+    {/each}
+  {/if}
+</div>
+
+<style lang="scss">
+  .pebbles-holder {
+    position: absolute;
+    width: 100%;
+    overflow: visible;
+    height: 30vh;
+    bottom: 0;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
+import PebblesContainer from "../components/pebbles-container.svelte";
 import Header from "../components/header.svelte";
 import MapikitLogo from "../components/mapikit-logo.svelte";
-import Pebbles from "../components/pebble.svelte";
 
 </script>
 
@@ -10,7 +10,7 @@ import Pebbles from "../components/pebble.svelte";
   <Header />
   <MapikitLogo />
   <br>
-  <Pebbles xStart={10} xSpread={80} yStart={20} ySpread={3}/>
+  <PebblesContainer />
   <br>
   <h1> Coming Soon... </h1>
 </body>
@@ -19,17 +19,25 @@ import Pebbles from "../components/pebble.svelte";
   body {
     text-align: center;
     font-family: Dosis;
+    overflow: hidden;
   }
 
   h1 {
+    position: absolute;
+    margin-bottom: auto;
+    margin-left: auto;
+    bottom: calc(20% - 15px);
+    left: calc(50% - 3em);
     padding: 0 16px;
     font-size: 4em;
-    color: rgba(255, 255, 255, 0.055);
+    color: #171730;
+    z-index: 5;
   }
 
   @media only screen and (min-width: 768px) {
     h1 {
       font-size: 5.5em;
+      bottom: calc(15% - 15px);
     }
   }
 </style>
