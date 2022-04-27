a2c0467b8611203a131b8eb583e880e3e7d4b591
Fábio Meneses Junior <flgmjr@gmail.com>
2022-04-27 11:59:05 -0300

    fix transition animation

--- src/components/common/guide-text.svelte
+++ src/components/common/guide-text.svelte
@@ -1,16 +1,37 @@
 <script lang="ts">
   import { guideText } from "../../stores/layout-tabs-store";
-  import { fly } from "svelte/transition";
+  import { cubicInOut } from "svelte/easing";
 
   let currentText = "";
   guideText.subscribe((value) => {
     currentText = value;
   });
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type,max-lines-per-function
+  const customFly = (node, {
+    delay = 0,
+    duration = 400,
+    x = 0,
+    y = 0,
+  }) => {
+    return {
+      delay,
+      duration,
+      css: (t : number) : string => {
+        let easedOpacity = cubicInOut(t);
+        let easedX = cubicInOut((1 - t))  * x;
+        let easedY = cubicInOut((1 - t))  * y;
+
+        return `transform: translate(${easedX}px, ${easedY}px);
+			  opacity: ${easedOpacity}`;
+      },
+    };
+};
 </script>
 
 <div class="guide-text-container">
   {#key currentText}
-    <h1 class="guide-text" in:fly="{{ x: -50, duration: 460, delay: 160 }}" out:fly="{{ x: -50, duration: 150 }}">
+    <h1 class="guide-text" in:customFly="{{ x: -50, duration: 460, delay: 160 }}" out:customFly="{{ x: -50, duration: 150 }}">
       {currentText}
     </h1>
   {/key}
@@ -28,11 +49,18 @@
     font-family: "Dosis";
     font-size: 16px;
     font-weight: 900;
-    user-select: none;
-    -moz-user-select: none;
 
     right: 36px;
     top: 120px;
+
+    .guide-text {
+      position: absolute;
+      right: 0;
+      top: 0;
+      width: 550px;
+      user-select: none;
+      -moz-user-select: none;
+    }
 
     h1 {
       color: #2c2c3d;
