e178112c67ad663464c159fe888257ee81d2e73f
Fábio Meneses Junior <flgmjr@gmail.com>
2024-03-21 07:02:03 -0300

    Add logo :D

--- src/frontend/layout.svelte
+++ src/frontend/layout.svelte
@@ -4,8 +4,8 @@
 
 <main class="h-full w-full flex flex-col">
 
-  <section class="w-full bg-norbalt-400 h-12"> <!-- HEADER (TOPBAR) -->
-    <!-- logo goes here -->
+  <section class="w-full bg-norbalt-400 h-12 flex items-center p-1"> <!-- HEADER (TOPBAR) -->
+    <img class="h-8 w-8 ml-1.5" src="./logo-meta-system.svg" alt="meta-system logo"/>
   </section>
   
   <main class="flex-1 flex flex-row">
