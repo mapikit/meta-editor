8ccbffae160ad322f0fb6e5669a413056a92fe81
Fábio Meneses Junior <flgmjr@gmail.com>
2024-05-09 22:24:02 -0300

    Small visual fixes Hub view

--- src/frontend/layout.svelte
+++ src/frontend/layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-
+  import MetaSystemIcon from "./icons/meta-system-icon.svelte";
 </script>
 
 <main class="h-full w-full flex flex-col">
 
   <section class="w-full bg-norbalt-400 h-12 flex items-center p-1"> <!-- HEADER (TOPBAR) -->
-    <img class="h-8 w-8 ml-1.5" src="./logo-meta-system.svg" alt="meta-system logo"/>
+    <div class="w-10 h-12"> <!-- Icon holder-->
+      <MetaSystemIcon width={1.8} />
+    </div>
   </section>
 
   <main class="flex-1 flex flex-row">
--- src/frontend/views/hub/hub.svelte
+++ src/frontend/views/hub/hub.svelte
@@ -1,40 +1,43 @@
 <script lang="ts">
-    import { get } from "svelte/store";
-    import { FileSystemController } from "../../../entities/controllers/file-system-controller";
-    import projectsStore from "../../../entities/stores/projects-store";
-    import ProjectPreviewCard from "./project-preview-card.svelte";
-    import MetaSystemIcon from "../../icons/meta-system-icon.svelte";
-    import { navigation } from "../../lib/navigation";
+  import { get } from "svelte/store";
+  import { FileSystemController } from "../../../entities/controllers/file-system-controller";
+  import projectsStore from "../../../entities/stores/projects-store";
+  import ProjectPreviewCard from "./project-preview-card.svelte";
+  import MetaSystemIcon from "../../icons/meta-system-icon.svelte";
+  import { navigation } from "../../lib/navigation";
+    import { getTailwindColor } from "../../../common/tailwind-configuration-accessor";
 
-    function navigateToProjects () : void {
-      navigation.navigateTo("/projects");
-    }
+  function navigateToProjects () : void {
+    navigation.navigateTo("/projects");
+  }
 </script>
 
-<div class="pt-6 w-full h-full bg-gradient-to-b from-norbalt-600 to-norbalt-400">
-<div class="grid flex-wrap w-5/6 mx-auto self-center">
-<span class="justify-center stroke-2 h-60 pb-10"><MetaSystemIcon/></span>
+<div class="pt-6 w-full h-full flex-col bg-global-bg-gradient flex items-center">
+<div class="flex flex-col flex-wrap w-full max-w-6xl px-8 self-center">
+<span class="justify-center stroke-2 h-60 pb-10"><MetaSystemIcon color={getTailwindColor("norbalt", 300)}
+  width={1.3}/></span>
 <div>
-    <div class="inline-flex w-full">
-        <div class="text-3xl font-semibold text-white">Recent Projects</div>
-        <span aria-hidden="true"
-        class="bg-norbalt-400 rounded font-bold p-2 text-base text-w text-offWhite mr-0 ml-auto hover:bg-norbalt-300 select-none cursor-pointer" on:click={navigateToProjects}>Open All Projects > </span>
-    </div>
-    <div class="flex flex-nowrap w-full py-4 space-x-4">
-        {#await FileSystemController.projectsController.loadAll()}
-        Loading Recent Projects...
-        {:then}
-            {#each get(projectsStore.items) as project}
-                <ProjectPreviewCard project={project}/>
-            {/each}
-        {/await}
-    </div>
+  <div class="inline-flex w-full">
+      <div class="text-3xl font-semibold text-white">Recent Projects</div>
+      <span aria-hidden="true"
+      class="bg-norbalt-400 rounded font-bold p-2 text-base text-w text-offWhite mr-0 ml-auto
+      hover:bg-norbalt-300 select-none cursor-pointer" on:click={navigateToProjects}>Open All Projects > </span>
+  </div>
+  <div class="flex flex-nowrap w-full py-4 space-x-4">
+      {#await FileSystemController.projectsController.loadAll()}
+      Loading Recent Projects...
+      {:then}
+          {#each get(projectsStore.items) as project}
+              <ProjectPreviewCard project={project}/>
+          {/each}
+      {/await}
+  </div>
 </div>
 <div>
-    <div class="text-3xl font-semibold text-white">Browse Plugins</div>
-    <div class="text-offWhite">
-        Coming Soon...
-    </div>
+  <div class="text-3xl font-semibold text-white">Browse Plugins</div>
+  <div class="text-offWhite">
+      Coming Soon...
+  </div>
 </div>
 </div>
 </div>
