c409f1ee41934a9a79bb61d4ceb02b8219532013
Fabio <flgmjr@gmail.com>
2024-05-12 08:51:12 -0300

    Finish projects page styling and add navigation back button

--- src/frontend/views/projects.svelte
+++ src/frontend/views/projects.svelte
@@ -3,18 +3,18 @@
 	import { FileSystemController } from "../../entities/controllers/file-system-controller.js";
 	import projectsStore from "../../entities/stores/projects-store.js";
   import MetaSystemIcon from "../icons/meta-system-icon.svelte";
-  import { getTailwindColor } from "../../common/tailwind-configuration-accessor";
   import TextButton from "../components/text-button.svelte";
+  import { navigation } from "../lib/navigation";
 
   const projects = projectsStore.items;
 </script>
 
 <section class="flex flex-col items-center w-full">
-  <div class="max-w-6xl w-full px-3 flex flex-col"> <!-- Content container-->
-    <div class="flex flex-row mt-16 items-center">
-      <div class="w-12 h-12 mb-1">
-        <MetaSystemIcon color={getTailwindColor("norbalt", 200)} width={2}/>
-      </div>
+  <div class="max-w-6xl w-full px-8 flex flex-col"> <!-- Content container-->
+    <div class="flex flex-row mt-10 items-center">
+      <button class="w-12 h-12 mb-1 stroke-norbalt-200 fill-norbalt-200" on:click={() => {navigation.navigateTo("/");}}>
+        <MetaSystemIcon width={2} style="hover:stroke-norbalt-100 hover:fill-norbalt-100"/>
+      </button>
       <h1 class="font-[Livvic] ml-6 text-3xl font-semibold" > Your Projects </h1>
       <TextButton text="Create" clickFunction={() => {}}/>
       <TextButton text="Import From Folder" lessMargin clickFunction={() => {}}/>
@@ -22,7 +22,7 @@
     {#await FileSystemController.projectsController.loadAll()}
     Loading Projects...
     {:then result}
-    <div class="flex flex-wrap">
+    <div class="flex flex-wrap -ml-3 mt-2">
       {#each $projects as project}
        <ProjectCard project={project}/>
       {/each}
